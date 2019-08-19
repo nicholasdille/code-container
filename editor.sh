ad384916f113c287f9ba10c175849ea4158175f0
Nicholas Dille <nicholas.dille@mailbox.org>
2019-08-19 13:12:39 +0200

    Entrypoint improvements

---
+++ editor.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+printenv
+
+# Project
+if mountpoint /files >/dev/null 2>&1; then
+    mkdir -p "/files/project"
+    ln -sf "/files/project" "/home/${EDITOR_USER_NAME}/project"
+else
+    mkdir -p "/home/${EDITOR_USER_NAME}/project"
+fi
+cp -f /completion.sh "/home/${EDITOR_USER_NAME}/.bash_completion"
+cd "/home/${EDITOR_USER_NAME}/project"
+if [ ! -z "${EDITOR_CLONE}" ]; then
+    git clone "${EDITOR_CLONE}" || true
+fi
+
+# Profile
+touch /tmp/.versions
+docker -v >> /tmp/.versions
+docker-compose -v >> /tmp/.versions
+echo ". /welcome.sh" >> "/home/${EDITOR_USER_NAME}/.bashrc"
+
+# Extensions
+if [ ! -z "${EDITOR_EXTENSIONS}" ]; then
+    IFS=";"
+    for EXTENSION in ${EDITOR_EXTENSIONS}; do
+        code-server --install-extension "${EXTENSION}" || true
+    done
+fi
+
+# Settings
+EDITOR_LINE_ENDINGS="${EDITOR_LINE_ENDINGS:-LF}"
+if [ "${EDITOR_LINE_ENDINGS}" != "CRLF" ]; then
+    git config --global core.autocrlf false
+    mkdir -p /home/editor/.local/share/code-server/User
+    echo '{"files.eol": "\n", "terminal.integrated.shell.linux": "/bin/bash"}' > /home/editor/.local/share/code-server/User/settings.json
+fi
+
+# Launch
+EDITOR_PORT="${EDITOR_PORT:-8443}"
+if [ ! -z "${EDITOR_PASSWORD}" ]; then
+    export PASSWORD="${EDITOR_PASSWORD}"
+    code-server --port "${EDITOR_PORT}" --allow-http
+else
+    code-server --port "${EDITOR_PORT}" --allow-http --no-auth
+fi
